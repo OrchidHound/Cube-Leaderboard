d6e7ecaea0d1fe91d1cc205ee48f61a64a6e0720
OrchidHound <MerynHogan@gmail.com>
2023-12-17 13:54:24 -0500

    Initial commit
    Implemented some menu options from https://guide.pycord.dev/interactions/ui-components/dropdowns in scripts/menu.py.
    Added a main.py.
    Added config.txt to .gitignore.

---
+++ main.py
@@ -0,0 +1,19 @@
+from discord import app_commands
+from discord.ext import commands
+from scripts import menu
+# from scripts import sql
+# from config import config
+import discord
+
+if __name__ == '__main__':
+    # Discord setup
+    TOKEN = config.TOKEN
+    bot = commands.Bot(command_prefix='>', intents=discord.Intents.all())
+
+    # Login confirmation
+    async def on_ready():
+        print("Logged in as {0.user}".format(bot))
+        # Sync the command tree and allow slash commands
+        await bot.tree.sync()
+
+    
---
+++ scripts/menu.py
@@ -0,0 +1,32 @@
+import discord
+
+bot = discord.Bot()
+
+
+class round_winner(discord.ui.View):
+    def __init__(self, player_one, player_two, round_number):
+        super().__init__()
+        self.player_one = player_one
+        self.player_two = player_two,
+        self.round_number = round_number
+
+    def make_list(self):
+        @discord.ui.select(
+            placeholder=f"Who won round {self.round_number}?",
+            min_values=1,
+            max_values=1,
+            options=[
+                discord.SelectOption(
+                    label=f"{self.player_one}"
+                ),
+                discord.SelectOption(
+                    label=f"{self.player_two}"
+                ),
+                discord.SelectOption(
+                    label="Draw",
+                    description="Either the round ended in a draw or there wasn't enough time to play the round."
+                )
+            ]
+        )
+        async def select_callback(self, select, interaction):
+            await interaction.response.send_message(f"{select.values[0]} won round {self.round_number}.")
